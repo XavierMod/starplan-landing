676ed6a4ba4a26feb277a485f9c5d403d47975d0
Xavier Mod <xaviermod@Xaviers-MacBook-Pro.local>
2020-07-17 16:24:38 +0200

    bugs

--- src/components/About/Team.js
+++ src/components/About/Team.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'gatsby'
 
 const TeamWrapper = styled.div`
     margin: 30px 0;
@@ -33,6 +34,35 @@ const TeamSlide = styled.div`
     }
 `;
 
+const TeamSlideOpen = styled.div`
+    text-align: left;
+    background: none;
+    padding: 20px;
+    margin: 15px;
+    display: inline-block;
+    width: 300px;
+    max-width: 400px;
+    border: 1px solid white;
+    p {
+        font-size: 12px;
+        margin-top: 10px;
+    }
+
+    span {
+        display: inline-block;
+        margin: 10px 0;
+        color: white;
+        font-size: 12px;
+        text-decoration: underline;
+        cursor: pointer;
+    }
+`;
+
+const Disclaimer = styled.p`
+    font-size: 11px;
+    margin: 60px;
+`;
+
 const Team = () => {
     return (
         <TeamWrapper>
@@ -41,15 +71,26 @@ const Team = () => {
             <TeamSlide>
                 <a target="_blank" href="https://www.xaviermod.com">
                     <h3>Xavier Mod</h3>
-                    <p>Founder, Front-End Design and Development</p>
+                    <p>Founder, Front-End Design and Development Lead, Project Manager. - Student*</p>
                     <span>Check profile</span>
                 </a>
             </TeamSlide>
             <TeamSlide>
                 <h3>Aidan Gannon</h3>
-                <p>Co-founder, Back-End Development</p>
+                <p>Co-founder, Back-End Development & Server Architecture Lead - Student*</p>
                 <span>Check profile</span>
             </TeamSlide>
+            <TeamSlideOpen>
+                <h3>Open Position!</h3>
+                <p>Art Direction Lead, Assets & Graphics Designer - Student*</p>
+                <span><Link to="/contact">Join us</Link></span>
+            </TeamSlideOpen>
+            <TeamSlideOpen>
+                <h3>Open Position!</h3>
+                <p>Digital Marketing, Digital Strategy Lead - Student*</p>
+                <span><Link to="/contact">Join us</Link></span>
+            </TeamSlideOpen>
+            <Disclaimer>*Starplan is an independent project run by University students. Revenue is not our primary goal, we do this for researching and team experience purposes. We do not have salaries, but we offer a % of equity depending on the position.</Disclaimer>
         </TeamWrapper>
     )
 }
